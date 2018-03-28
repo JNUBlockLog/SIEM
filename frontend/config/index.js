ad93bdebd22c8689f40890f90a473e1eb57d2664
HardPlant <abc7988se@naver.com>
2018-03-28 22:39:29 +0900

    update

---
+++ frontend/config/index.js
@@ -0,0 +1,5 @@
+build: { // Template for index.html
+index: path.resolve(__dirname, '../../src/main/resources/templates/index.html'),
+// Assets Paths
+assetsRoot: path.resolve(__dirname, '../../src/main/resources/static'),
+}
---
+++ src/main/java/com/blocklogsiem/blocklog_siem/PostsController.java
@@ -0,0 +1,17 @@
+package com.blocklogsiem.blocklog_siem;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@EnableAutoConfiguration
+public class PostsController {
+    @RequestMapping(value = "/posts/new", method = RequestMethod.GET)
+    public String newPost(Model model) {
+        model.addAttribute("post", new Post());
+        return "new";
+    }
+}
