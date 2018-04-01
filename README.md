5668b4c2fbdd657e651bf3e562cc7cd016d1e4ac
HardPlant <abc7988se@naver.com>
2018-04-01 21:05:56 +0900

    update

--- README.md
+++
@@ -1,6 +0,0 @@
-# SIEM
-
-Simple SIEM
-
-[](http://itstory.tk/entry/Spring-Boot-Vuejs-%EC%97%B0%EB%8F%99%ED%95%98%EA%B8%B0)
-[](http://blog.saltfactory.net/creating-springboot-project-in-intellij/)
--- frontend/config/index.js
+++
@@ -1,5 +0,0 @@
-build: { // Template for index.html
-index: path.resolve(__dirname, '../../src/main/resources/templates/index.html'),
-// Assets Paths
-assetsRoot: path.resolve(__dirname, '../../src/main/resources/static'),
-}
